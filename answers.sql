68821ecb8ffaf575ab50f97237aaba77a199f1cb
Oldřich Veselý <olda335@gmail.com>
2023-06-27 18:06:52 +0200

    answers

--- answers.sql
+++ answers.sql
@@ -1,28 +1,5 @@
 -- otázka č.1 míra poklesu mezd ve sledované období
 
-SELECT vodp.year_payroll AS aktual_year , vodp.payroll AS aktual, vodp2.payroll AS prev ,vodp2.year_payroll AS year_prev, vodp.branch  ,
-round((vodp.payroll - vodp2.payroll)/vodp2.payroll*100,2) AS growth
-FROM v_oldrich_vesely_project_sql_primary_final vodp 
-JOIN v_oldrich_vesely_project_sql_primary_final vodp2 
-	ON vodp.year_payroll = vodp2.year_payroll + 1
-	AND vodp.code_branch = vodp2.code_branch 
-GROUP BY vodp.year_payroll, vodp.code_branch 
-ORDER BY growth;
-
--- výběr odvětví, ve kterých byl někdy pokles mezd
-
-SELECT DISTINCT down.branch
-FROM 
-(SELECT vodp.branch AS branch, 
-round((vodp.payroll - vodp2.payroll)/vodp2.payroll*100,2) AS growth
-FROM v_oldrich_vesely_project_sql_primary_final  vodp 
-JOIN v_oldrich_vesely_project_sql_primary_final  vodp2 
-	ON vodp.year_payroll = vodp2.year_payroll + 1
-	AND vodp.code_branch = vodp2.code_branch 
-GROUP BY vodp.year_payroll, vodp.code_branch 
-HAVING growth < 0
-ORDER BY vodp.branch ) down
-
 -- pomocná tabulka se zobrazením meziročních změn mezd
 
 CREATE VIEW v_growth_payroll AS 
@@ -35,19 +12,26 @@ JOIN v_oldrich_vesely_project_sql_primary_final vodp2
 GROUP BY vodp.year_payroll, vodp.code_branch 
 ORDER BY growth_percent;
 
--- porovnání seznamu odvětví a odvětví, které nikdy neměly pokles, odvětví NULL - stále rostly
+-- výběr odvětví, ve kterých byl někdy pokles mezd
 
-SELECT vgp2.branch, vgp.branch 
-FROM v_growth_payroll vgp2 
-LEFT JOIN v_growth_payroll vgp
-	ON vgp2.branch = vgp.branch
- 	AND vgp2.growth_percent <0 
-GROUP BY vgp2.branch;
+SELECT DISTINCT branch
+FROM v_growth_payroll vgp 
+WHERE growth_percent < 0
+
+-- výběr odvětví, které nikdy neměly pokles mezd 
+
+SELECT DISTINCT branch
+FROM v_growth_payroll vgp 
+WHERE branch NOT IN (
+SELECT DISTINCT branch
+FROM v_growth_payroll vgp 
+WHERE growth_percent < 0);
+
 
 -- otázka č.2 nákup kg chleba a litry mléka v první a posledním sledovaném období
 
 
-SELECT vodp.price , vodp.item, vodp.year_price , round(avg(vodp.payroll ),2) AS salary , round(avg(vodp.payroll)/vodp.price,2) AS quantity, vodp.unit
+SELECT vodp.price, vodp.item, vodp.year_price , round(avg(vodp.payroll ),2) AS salary , round(avg(vodp.payroll)/vodp.price,2) AS quantity, vodp.unit
 FROM v_oldrich_vesely_project_sql_primary_final vodp
 WHERE vodp.item IN ('Mléko polotučné pasterované', 'Chléb konzumní kmínový')
 AND vodp.year_price IN (2006,2018)
